ae7778356e74f065ade14b4a3054713ee224cea3
GodFlight <prokushevmaksim@gmail.com>
2019-09-15 22:23:31 +0300

    added plane texture processing

--- include/rt_cl.h
+++ include/rt_cl.h
@@ -34,6 +34,7 @@ float	sphereSDF(float3 posc, float radius);
 
 float2	uv_mapping_for_sphere(t_raycast_hit rh);
 float2	uv_mapping_for_cylinder(t_raycast_hit rh);
+float2	uv_mapping_for_plane(t_raycast_hit rh);
 
 char	raymarch(float3 origin, float3 direction, float distance, __global t_scene *scene, t_raycast_hit *rh);
 
