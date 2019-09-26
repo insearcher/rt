a868c22b5cc264c419f11355f576236644493556
frolushka <mpfrolovstud@gmaill.com>
2019-09-26 10:04:37 +0300

    Rename setup_rt to rt_setup (WTF always naming functions *project*_*function*) On my tests vectors works fine

--- src/main.c
+++ src/main.c
@@ -119,7 +119,7 @@ int main()
 
 	rt_screen_size = modification_rt_elem_and_get_screen_size(ui);
 
-	rt = setup_rt(rt_screen_size);
+	rt = rt_setup(rt_screen_size, "json/textures.json", "json/scenes/mandelbulb.json");
 	ui->data = (void *)rt;
 //	rt->params |= RT_GAUSS_BLUR;
 	rt->scene->params |= RT_PATH_TRACE;
