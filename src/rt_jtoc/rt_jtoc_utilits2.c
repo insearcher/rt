129d06429df081739183eb3156ea42edc48b2675
frolushka <mpfrolovstud@gmail.com>
2019-12-01 14:36:45 +0300

    norme 2/5 src folder. still work

---
+++ src/rt_jtoc/rt_jtoc_utilits2.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rt_jtoc_utilits2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbednar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/26 14:15:06 by sbednar           #+#    #+#             */
+/*   Updated: 2019/09/26 14:15:43 by sbednar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+int				rt_jtoc_sdl_log_error(const char *p, const int id)
+{
+	SDL_Log("%s ----> ERROR <---- %s", KRED, KNRM);
+	SDL_Log("INCORRECT: %s%s%s%s%s",
+			p,
+			id < 0 ? "" : " IN ID = ",
+			KGRN,
+			id < 0 ? "" : ft_itoa(id),
+			KNRM);
+	return (FUNCTION_FAILURE);
+}
+
+int				rt_jtoc_get_objects_num_in_arr(unsigned int *on, t_jnode *n)
+{
+	t_jnode	*tmp;
+
+	if (n == NULL)
+		return (FUNCTION_SUCCESS);
+	if (n->type != array)
+		return (rt_jtoc_sdl_log_error("TYPE IS NOT ARRAY", -1));
+	tmp = n->down;
+	while (tmp)
+	{
+		if (tmp->type != object)
+			return (rt_jtoc_sdl_log_error("TYPE IS NOT OBJECT", -1));
+		(*on)++;
+		tmp = tmp->right;
+	}
+	return (FUNCTION_SUCCESS);
+}
+
+int				rt_jtoc_get_float2(cl_float2 *vec, t_jnode *n)
+{
+	t_jnode	*tmp;
+
+	if (!(tmp = jtoc_node_get_by_path(n, "x")) || tmp->type != fractional)
+		return (rt_jtoc_sdl_log_error("X ERROR", -1));
+	vec->x = jtoc_get_float(tmp);
+	if (!(tmp = jtoc_node_get_by_path(n, "y")) || tmp->type != fractional)
+		return (rt_jtoc_sdl_log_error("Y ERROR", -1));
+	vec->y = jtoc_get_float(tmp);
+	return (FUNCTION_SUCCESS);
+}
+
+int				rt_jtoc_get_float3(cl_float3 *vec, t_jnode *n)
+{
+	t_jnode	*tmp;
+
+	if (!(tmp = jtoc_node_get_by_path(n, "x")) || tmp->type != fractional)
+		return (rt_jtoc_sdl_log_error("X ERROR", -1));
+	vec->x = jtoc_get_float(tmp);
+	if (!(tmp = jtoc_node_get_by_path(n, "y")) || tmp->type != fractional)
+		return (rt_jtoc_sdl_log_error("Y ERROR", -1));
+	vec->y = jtoc_get_float(tmp);
+	if (!(tmp = jtoc_node_get_by_path(n, "z")) || tmp->type != fractional)
+		return (rt_jtoc_sdl_log_error("Z ERROR", -1));
+	vec->z = jtoc_get_float(tmp);
+	return (FUNCTION_SUCCESS);
+}
